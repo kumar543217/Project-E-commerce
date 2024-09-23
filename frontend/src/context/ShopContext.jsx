7fb53792d144c40bd7080e242796aca4ef86e5a8
Deepak kumar <dee96872@gmail.com>
2024-09-23 22:45:24 +0530

    i add function for updaste card item and also remove

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -40,6 +40,18 @@ const ShopContextProvider = (props) => {
             cartData[itemId][size] = 1;
         }
         setCartItems(cartData);
+
+        if (token) {
+            try {
+
+                await axios.post(backendUrl + "/api/cart/add", { itemId, size }, { headers: { token } })
+
+            } catch (error) {
+                console.log(error);
+                toast.error(error.message);
+
+            }
+        }
     }
 
     const getCartCount = () => {
@@ -64,6 +76,17 @@ const ShopContextProvider = (props) => {
         cartData[itemId][size] = quantity;
 
         setCartItems(cartData);
+
+        if (token) {
+            try {
+
+                await axios.post(backendUrl + '/api/cart/update', { itemId, size, quantity }, { headers: { token } })
+
+            } catch (error) {
+                console.log(error);
+                toast.error(error.message);
+            }
+        }
     }
 
     const getCartAmount = () => {
@@ -98,13 +121,26 @@ const ShopContextProvider = (props) => {
         }
     }
 
+    const getUserCart = async (token) => {
+        try {
+            const response = await axios.post(backendUrl + '/api/cart/get', {}, { headers: { token } })
+            if (response.data.success) {
+                setCartItems(response.data.cartData);
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message)
+        }
+    }
+
     useEffect(() => {
         getProductsData()
     }, [])
 
     useEffect(() => {
-        if(!token && localStorage.getItem("token")){
+        if (!token && localStorage.getItem("token")) {
             setToken(localStorage.getItem('token'))
+            getUserCart(localStorage.getItem("token"));
         }
     }, [])
 
