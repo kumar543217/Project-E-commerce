fdf27b084eabb57664df8546ce3d5a5b58feb563
Deepak kumar <dee96872@gmail.com>
2024-09-24 23:47:49 +0530

    i add sample.env files

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,35 @@
+
+
+// Placing order using COD Method
+const placeOrder = async(req, res)=>{
+
+}
+
+// Placing orders using Stripe Method
+const placeOrderStripe = async(req, res)=>{
+
+}
+
+//Placing orders using Razorpay Method
+const placeOrderRezorpay = async(req, res)=>{
+
+}
+
+// All Orders data for Adin Panel
+const allOrders = async (req, res)=>{
+
+}
+
+// User Order Data For Forntend
+const userOrders =async(req, res)=>{
+
+}
+
+// Update orders status from Admin Panel
+const updateStatus = async (req, res)=>{
+
+}
+
+export {placeOrder, placeOrderStripe, placeOrderRezorpay, 
+ allOrders, userOrders, updateStatus   
+}
